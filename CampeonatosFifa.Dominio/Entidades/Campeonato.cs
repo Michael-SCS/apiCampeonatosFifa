a41f61a42b0f33ec864574138a500ff482546d6c
Michael Steven Castillo <michaelsteven3211@gmail.com>
2024-02-12 07:28:56 -0500

    first commit

---
+++ CampeonatosFifa.Dominio/Entidades/Campeonato.cs
@@ -0,0 +1,18 @@
+﻿namespace CampeonatosFifa.Dominio.Entidades
+{
+    public class Campeonato
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Campeonato"), StringLength(100)]
+        public required String Nombre { get; set; }
+
+        [Column("IdPais")]
+        public int IdSeleccion {get; set;}
+
+        public Seleccion Seleccion { get; set;}
+        [Column("Año")]
+        public int Año { get; set; }
+    }
+}
---
+++ CampeonatosFifa.Dominio/Entidades/Seleccion.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CampeonatosFifa.Dominio.Entidades
+{
+    [Table("Pais")]
+    public class Seleccion
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Pais"), StringLength(100)]
+        public required String Nombre { get; set; }
+
+        [Column("Entidad"), StringLength(100)]
+        public required String Entidad { get; set; }
+    }
+}
